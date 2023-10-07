a7627fbad25ac8202b6298dfe7008ace8d5e2fa9
WoWdeadlock <chandithamahanama@gmail.com>
2023-10-08 00:09:23 +0530

    completed functionality

--- js/script.js
+++ js/script.js
@@ -1,23 +1,65 @@
-password = document.querySelector(".password");
-passwordConfirm = document.querySelector(".confirm-password");
+password = document.querySelector("#password-value");
+passwordConfirm = document.querySelector("#confirm-password-value");
 
 passwordInvalid = document.querySelector(".invalid-password");
 fieldsInvalid = document.querySelector(".invalid-fields");
 
 createButton = document.querySelector("#create-button");
 
+inputBoxes = document.querySelectorAll(".input-value");
+
 createButton.addEventListener("click", () => {
 
-    if (password.innerText !== passwordConfirm.innerText) {
+    emptyField = true;
+
+    if (password.value !== passwordConfirm.value) {
 
         passwordInvalid.classList.remove("hide");
 
     } else {
 
+        passwordInvalid.classList.add("hide");
 
-            passwordInvalid.classList.add("hide");
+    }
 
+    for (let i = 0; i < inputBoxes.length; i++) {
+
+        if (inputBoxes[i].value.trim().length === 0) {
+
+            inputBoxes[i].classList.add("invalid-border");
+            emptyField = true;
+            break;
+
+        } else {
+
+            emptyField = false;
+
+        }    
+
+    }
+
+    if (emptyField == true) {
+
+        fieldsInvalid.classList.remove("hide");
+
+    } else {
+
+        fieldsInvalid.classList.add("hide");
 
     }
 
 });
+
+for (let i = 0; i < inputBoxes.length; i++) {
+
+    inputBoxes[i].addEventListener("click", () => {
+
+        if (inputBoxes[i].classList.contains("invalid-border")) {
+
+            inputBoxes[i].classList.remove("invalid-border");
+    
+        }            
+
+    })
+
+}
